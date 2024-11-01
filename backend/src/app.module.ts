7f78a943cf65410e97bae95a0453abf15e5c828b
ms-dev97 <mohammed.salem701@gmail.com>
2024-11-01 23:21:42 +0300

    add new modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule, AuthModule, BooksModule, AuthorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
